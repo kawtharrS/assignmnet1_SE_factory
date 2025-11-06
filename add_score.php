dfce02b9b0178f87fe27d1584ebe79f43eb0c4d2
Kawthar Shaalan <101220311@mu.edu.lb>
2025-11-06 05:25:01 +0200

    add-score api not final conditions

--- add_score.php
+++ add_score.php
@@ -1,3 +1,37 @@
 <?php
-    if($_Server)
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Headers: Content-Type");
+    header("Access-Control-Allow-Methods: POST");
+    header("Content-Type: application/json");
+
+
+    include("connection.php");
+    $data = json_decode(file_get_contents("php://input"), true);
+
+    $name=$data["name"];
+    $score=$data["score"];
+    $duration=$data["duration"];
+
+
+    $sql = "INSERT INTO players (name, score, duration) VALUES (?,?,?)";
+    $query=$mysql->prepare($sql);
+    $query->bind_param("sii", $name, $score, $duration);
+
+
+    if ($query->execute()) {
+    echo json_encode(["status" => "success"]);
+    } else {
+        echo json_encode(["status" => "error"]);
+    }
+
+    $check = $mysql->prepare("SELECT id FROM player WHERE name = ?");
+    $check->bind_param("s", $name);
+    $check->execute();
+    $result = $check->get_result();
+
+    if ($result->num_rows > 0) {
+        echo json_encode(["status" => "duplicate"]);
+        exit;
+    }
+
 ?>
